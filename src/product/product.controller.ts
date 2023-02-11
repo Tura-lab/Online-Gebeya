5e81e0641e6f2a26a076decfbba2c56c6ab4ee50
Yeabtse <Yeabtse7@gmail.com>
2023-02-11 11:29:17 -0900

    Edited product module

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,34 @@
 import { Controller,Post, Get, Put, Delete, Body, Param, Query, NotFoundException } from '@nestjs/common';
-// import { generate } from 'rxjs';
 import { ProductService } from './product.service';
-
+import { Product } from 'src/types/product';
 @Controller('product')
 export class ProductController {
     constructor(private productService: ProductService) { }
 
+    @Post()
+  async create(@Body() product: Product) {
+    return this.productService.createProduct(product);
+  }
 
+  @Get()
+  async findAll(): Promise<Product[]> {
+    return this.productService.getAllProduct();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.productService.getProductById(id);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() product: Product) {
+    return this.productService.updateProduct(id, product);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.productService.deleteProduct(id);
+  }
 
 }
 
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { ProductSchema } from 'src/models/product.schema';
 import { Product } from 'src/types/product';
-
+import { BadRequestException } from '@nestjs/common';
 
 
 @Injectable()
 export class ProductService {
-
-
   constructor(@InjectModel('Product') private readonly productModel: Model<Product>) { }
 
+  async createProduct(product: Product): Promise<Product> {
+    const createdProduct = new this.productModel(product);
+    return await createdProduct.save();
+  }
 
+  async getAllProduct(): Promise<Product[]> {
+    return await this.productModel.find().exec();
+  }
 
+  async getProductById(id: string): Promise<Product> {
+    // if (!(id in this.productModel)) {
+    //   throw new BadRequestException('Product not found');
+    // }
+    return await this.productModel.findById(id).exec();
+  }
+
+  async updateProduct(id: string, product: Product): Promise<Product> {
+    return await this.productModel.findByIdAndUpdate(id, product, { new: true });
+  }
+
+  async deleteProduct(id: string): Promise<Product> {
+    return await this.productModel.findByIdAndRemove(id);
+  }
 }
