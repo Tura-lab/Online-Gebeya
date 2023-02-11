3e45b13ce8ebeeab6c5da9ac293dafc5f25e759e
Yeabtse <Yeabtse7@gmail.com>
2023-02-11 06:35:54 -0900

    auth done

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,21 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
+import { UserSchema } from 'src/models/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt/dist';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    JwtModule.register({
+      secret: 'abcdABCD1234554321',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/types/user';
+import { JwtService } from '@nestjs/jwt';
+import { UserSchema } from 'src/models/user.schema';
+import { UserJwtPayload } from 'src/types/jwt-payload';
 
-
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectModel('User') private readonly userModel: Model<User>) { }
+  constructor(
+    @InjectModel('User') private readonly userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signin(user): Promise<{ accessMessage: string }> {
+    const foundUser = await this.userModel.findOne({
+      email: user.email
+    });
+
+    if (foundUser) {
+      const isMatch = await bcrypt.compare(user.password, foundUser.password);
+      console.log(foundUser)
+
+      if (!isMatch) {
+        throw new UnauthorizedException('Incorrect login credentials!');
+      }
+
+      console.log(foundUser);
+      const typeid = user.id;
+      const payload: UserJwtPayload = { username: user.email, typeid };
+      const accessMessage: string = await this.jwtService.sign(payload);
 
 
+      return { accessMessage };
+    } else {
+      throw new UnauthorizedException('Incorrect login credentials!');
+    }
+  }
+
+  async createUser(user): Promise<User> {
+    const foundUser = await this.userModel.findOne({ email: user.email });
+
+
+    if (foundUser) {
+      throw new BadRequestException('Email already used!');
+    }
+
+    const salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password, salt);
+
+    const newUser = await (await this.userModel.create(user)).save();
+    
+    return newUser;
+  }
 }
