e3f8c4d8a5f5798d1b8d684d8cf2053a2edb2c28
Daniel Wossen <wossendaniel7@gmail.com>
2023-02-11 19:52:51 +0300

    upload logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,18 @@
 import { Module } from '@nestjs/common';
-import { RouterModule } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
+import mongoose from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 
+
 @Module({
-  imports: [
-    RouterModule.register([
-      {
-        path: 'users',
-        module: UserModule,
-      },
-      {
-        path: 'products',
-        module: ProductModule,
-      },
-    ]),
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017'),
-    ProductModule,
-    UserModule,
-  ],
+  imports: [MongooseModule.forRoot("mongodb+srv://admin:admin@cluster0.ssogond.mongodb.net/?retryWrites=true&w=majority"), ProductModule, UserModule, MulterModule.register({
+    dest:'./src/uploads',
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {} 
